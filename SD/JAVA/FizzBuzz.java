28dd465d0ab220fd0c9bccbf1d058e6d3eed2087
Tofayel Ahmmed <tofayel.ahmmed084@gmail.com>
2025-11-12 19:42:15 +0600

    Added.

---
+++ SD/JAVA/FizzBuzz.java
@@ -0,0 +1,26 @@
+/*
+    * Question: Write a program that prints the numbers from 1 to 100. But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".
+ */
+
+import java.util.Scanner;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the upper limit (n): ");
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
